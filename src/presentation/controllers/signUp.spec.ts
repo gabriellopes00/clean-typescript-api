aafdf71886d09b7e881e36164a62625d23c6e5c9
Gabriel Lopes <gabrielluislopes00@gmail.com>
2020-12-11 11:45:22 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -14,4 +14,18 @@ describe('SingUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'gabriel',
+        password: 'gabriel123'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,5 +1,10 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    return { statusCode: 400, body: new Error('Missing param: name') }
+    if (!httpRequest.body.name) {
+      return { statusCode: 400, body: new Error('Missing param: name') }
+    }
+    if (!httpRequest.body.email) {
+      return { statusCode: 400, body: new Error('Missing param: email') }
+    }
   }
 }
