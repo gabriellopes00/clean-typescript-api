a29a90e88e2b7f0a9e957fac646a4fede17df626
Gabriel Lopes <gabrielluislopes00@gmail.com>
2020-12-19 17:26:28 -0300

    feat: ensure CORS is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,9 @@
 import { Express } from 'express'
+
 import { bodyParser } from '../middlewares/bodyParser'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
