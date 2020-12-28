0bdce804889428ce278f5db21fddf0c644b76e55
Gabriel Lopes <gabrielluislopes00@gmail.com>
2020-12-28 20:04:14 -0300

    refactor: remove duplicated code in LogDecorator tests

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,12 +1,32 @@
 import { Controller, HttpResponse, HttpRequest } from '@presentation/interfaces'
 import { LogControllerDecorator } from './log'
-import { serverError } from '@presentation/helpers/http'
+import { serverError, ok } from '@presentation/helpers/http'
 import { LogErrorRepository } from '@data/interfaces/logErrorRepository'
+import { AccountModel } from '@domain/models/account'
 
 interface SutTypes {
   sut: LogControllerDecorator
   controllerStub: Controller
   logErrorRepositoryStub: LogErrorRepository
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'gabriel',
+    email: 'gabriel@example.com',
+    password: 'gabriel123'
+  }
+})
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid-id',
+  name: 'valid-name',
+  email: 'valid-email',
+  password: 'valid-password'
+})
+const makeFakeServerError = (): HttpResponse => {
+  const fakeError = new Error()
+  fakeError.stack = 'error_stack'
+  return serverError(fakeError)
 }
 
 const makeLogErrorRepository = (): LogErrorRepository => {
@@ -20,15 +40,7 @@ const makeLogErrorRepository = (): LogErrorRepository => {
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-      const httpResponse: HttpResponse = {
-        statusCode: 200,
-        body: {
-          email: 'gabriel@example.com',
-          name: 'Gabriel Lopes',
-          password: 'gabriel123'
-        }
-      }
-      return new Promise((resolve, reject) => resolve(httpResponse))
+      return new Promise((resolve, reject) => resolve(ok(makeFakeAccount())))
     }
   }
   return new ControllerStub()
@@ -45,59 +57,28 @@ describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const httpRequest: HttpRequest = {
-      body: {
-        email: 'gabriel@example.com',
-        name: 'Gabriel Lopes',
-        password: 'gabriel123'
-      }
-    }
 
-    await sut.handle(httpRequest)
-    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+    await sut.handle(makeFakeRequest())
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest())
   })
 
   test('Should return the same result of the controller', async () => {
     const { sut } = makeSut()
-    const httpRequest: HttpRequest = {
-      body: {
-        email: 'gabriel@example.com',
-        name: 'Gabriel Lopes',
-        password: 'gabriel123'
-      }
-    }
 
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual({
-      statusCode: 200,
-      body: {
-        name: 'Gabriel Lopes',
-        email: 'gabriel@example.com',
-        password: 'gabriel123'
-      }
-    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(makeFakeAccount()))
   })
 
   test('Should call LogError with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
 
-    const fakeError = new Error()
-    fakeError.stack = 'error_stack'
-    const error = serverError(fakeError)
-
     const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
     jest
       .spyOn(controllerStub, 'handle')
-      .mockReturnValueOnce(new Promise((resolve, reject) => resolve(error)))
-    const httpRequest: HttpRequest = {
-      body: {
-        email: 'gabriel@example.com',
-        name: 'Gabriel Lopes',
-        password: 'gabriel123'
-      }
-    }
-
-    await sut.handle(httpRequest)
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => resolve(makeFakeServerError()))
+      )
+    await sut.handle(makeFakeRequest())
     expect(logSpy).toHaveBeenCalledWith('error_stack')
   })
 })
