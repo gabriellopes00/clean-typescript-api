a37520f3b1ad208d5b9057d008d6e01695764b75
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-01-19 17:02:55 -0300

    chore: enable debug with docker and vscode

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 
 export default {
   mongoUrl:
-    process.env.MONGO_URL || 'mongodb://mongo:27017/clean-typescript-api',
+    process.env.MONGO_URL || 'mongodb://localhost:27017/clean-typescript-api',
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET_KEY || 'jw3R4fdaF74Fd7dfH'
 }
