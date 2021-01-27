81a4c7bafd92fa012392275c4571702275c429ed
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-01-27 12:44:52 -0300

    feat: ensure AuthMiddleware calls loadAccounByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,10 +1,28 @@
 import { forbidden } from '../helpers/http/http'
 import { AccessDeniedError } from '../errors/access-denied'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account'
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'gabriel',
+  email: 'gabrielluislopes00@gmail.com',
+  password: 'hashed_password'
+})
+
+const makeLoadAccountByTokenStub = () => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    load(accessToken: string): Promise<AccountModel> {
+      return new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
 const makeSut = () => {
-  const sut = new AuthMiddleware()
-  return { sut }
+  const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+  return { sut, loadAccountByTokenStub }
 }
 
 describe('Auth Middleware', () => {
@@ -12,5 +30,17 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    await sut.handle({
+      headers: {
+        accessToken: 'any_token'
+      }
+    })
+
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -2,10 +2,18 @@ import { AccessDeniedError } from '@presentation/errors/access-denied'
 import { HttpRequest, HttpResponse } from '@presentation/interfaces'
 import { Middleware } from '../interfaces/middleware'
 import { forbidden } from '../helpers/http/http'
+import { LoadAccountByToken } from '@domain/usecases/load-account-by-token'
 
 export class AuthMiddleware implements Middleware {
-  handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.headers?.accessToken
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken)
+    }
+
     const error = forbidden(new AccessDeniedError())
-    return new Promise(resolve => resolve(error))
+    return error
   }
 }
