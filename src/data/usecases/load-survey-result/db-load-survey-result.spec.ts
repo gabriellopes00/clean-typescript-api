92ac0e909c7db66b08698599cdd54a7fecbf0f13
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-03-08 13:40:26 -0300

    feat: ensure DbLoadSurveyResult throw LoadSurveyResultRepository throws

--- src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,10 +1,10 @@
 import { fakeSurveyResultModel } from '../../../domain/mocks/mock-survey-result'
 import { SurveyResultsModel } from '../../../domain/models/survey-results'
 import { LoadSurveyResultRepository } from '../../interfaces/db/survey/load-survey-results-repository'
-import { DbLoadSurveyResults } from './db-save-survey-result'
+import { DbLoadSurveyResults } from './db-load-survey-result'
 
 class MockLoadSurveyResultsRepository implements LoadSurveyResultRepository {
-  async loadBySurveyId(surveyId: string): Promise<SurveyResultsModel> {
+  async loadBySurveyId(fakeSurveyId: string): Promise<SurveyResultsModel> {
     return fakeSurveyResultModel
   }
 }
@@ -12,11 +12,17 @@ class MockLoadSurveyResultsRepository implements LoadSurveyResultRepository {
 describe('DbLoadSurveyResult Usecase', () => {
   const mockLoadSurveyResultsRepository = new MockLoadSurveyResultsRepository() as jest.Mocked<MockLoadSurveyResultsRepository>
   const sut = new DbLoadSurveyResults(mockLoadSurveyResultsRepository)
-  const surveyId = 'any_surveyId'
+  const fakeSurveyId = 'any_surveyId'
 
   test('Should call LoadSurveyResultRepository', async () => {
     const load = jest.spyOn(mockLoadSurveyResultsRepository, 'loadBySurveyId')
-    await sut.load(surveyId)
-    expect(load).toHaveBeenCalledWith(surveyId)
+    await sut.load(fakeSurveyId)
+    expect(load).toHaveBeenCalledWith(fakeSurveyId)
+  })
+
+  test('Should throw if SaveSurveyRepository throws', async () => {
+    mockLoadSurveyResultsRepository.loadBySurveyId.mockRejectedValueOnce(new Error())
+    const error = sut.load(fakeSurveyId)
+    await expect(error).rejects.toThrow()
   })
 })
