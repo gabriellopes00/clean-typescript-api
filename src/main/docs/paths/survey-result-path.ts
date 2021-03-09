660dc4b80484fad8bf8702ca3f2eb2a9cec350cb
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-03-09 10:22:10 -0300

    docs: add load survey result API docs

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -16,5 +16,20 @@ export const surveyResultPath = {
       404: { $ref: '#/components/notFound' },
       500: { $ref: '#/components/serverError' }
     }
+  },
+  get: {
+    security: [{ apiKeyAuth: [] }],
+    tags: ['Surveys'],
+    summary: 'Surveys result query API',
+    parameters: [{ in: 'path', name: 'surveyId', required: true, schema: { type: 'string' } }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: { 'application/json': { schema: { $ref: '#schemas/surveyResult' } } }
+      },
+      403: { $ref: '#/components/forbidden' },
+      404: { $ref: '#/components/notFound' },
+      500: { $ref: '#/components/serverError' }
+    }
   }
 }
