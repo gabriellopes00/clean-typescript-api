a92d8fda45795da504c575741cdaf9be283f3d56
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-03-26 13:39:37 -0300

    docs: update login and signup schema

--- src/main/docs/schemas/account-schema.ts
+++ src/main/docs/schemas/account-schema.ts
@@ -1,4 +1,5 @@
 export const accountSchema = {
   type: 'object',
-  properties: { accessToken: { type: 'string' } }
+  properties: { accessToken: { type: 'string' }, name: { type: 'string' } },
+  required: ['accessToken', 'name']
 }
